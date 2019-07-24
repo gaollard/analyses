9845f97fb88eecc076d85583183db09b4c327fd5
shiyuejs <313479200@qq.com>
2019-07-24 17:15:45 +0800

    部署

--- app/middleware/apiWapper.js
+++ app/middleware/apiWapper.js
@@ -3,9 +3,9 @@ module.exports = () => {
 		await next();
 
 		console.log(ctx.request.header, 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx')
-		ctx.set("Access-Control-Allow-Origin",　ctx.request.header.host);
+		ctx.set("Access-Control-Allow-Origin",　"*");
 		ctx.set("Access-Control-Allow-Methods","POST, GET, PUT, DELETE, OPTIONS");
-		ctx.set("Access-Control-Allow-Credentials",true);
+		ctx.set("Access-Control-Allow-Credentials",false);
 		ctx.set("Access-Control-Max-Age",'86400'); // 24 hours
 		ctx.set("Access-Control-Allow-Headers","X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
 	}
